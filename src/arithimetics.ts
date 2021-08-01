b69c873248511e1f641381c5a88cbfad25f439c5
Andre Torres <andre855@gmail.com>
2021-08-01 21:07:38 +0200

    Allow multiple digits
    Throw error for division by 0

--- src/arithimetics.ts
+++ src/arithimetics.ts
@@ -1,11 +1,52 @@
+const operations: Record<string, (left: number, right:number) => number> = {
+  "+": (left: number, right: number) => left + right,
+  "-": (left: number, right: number) => left - right,
+  "/": (left: number, right: number) => left / right,
+  "*": (left: number, right: number) => left * right,
+}
+
+const signals = new Set(["+", "-", "/", "*"]);
+
 export function calculate(operation: string): number {
   if (operation.length === 3) {
     return parseInt(operation.charAt(1));
   }
 
-  if (operation.charAt(2) === "+") {
-    return parseInt(operation.charAt(1)) + parseInt(operation.charAt(3));
-  } else {
-    return parseInt(operation.charAt(1)) - parseInt(operation.charAt(3));
+  let left: string = "";
+  let right: string = "";
+  let signal: string = "";
+
+  for (let i = 0; i < operation.length; i++) {
+    const char = operation.charAt(i);
+    
+    if (!isValid(char)) {
+      continue;
+    }
+
+    if (!signals.has(char)) {
+      if (signal === "") {
+        left = left + char;
+      } else {
+        right = right + char;
+      }
+    } else {
+      signal = char;
+    }
   }
+
+  if (signal === "/" && parseInt(right) === 0) {
+    throw new Error("division by zero not allowed");
+  }
+  return operations[signal](parseInt(left), parseInt(right));
+}
+
+
+function isValid(char: string): boolean {
+  const res = parseInt(char);
+
+  if (isNaN(res)) {
+    return signals.has(char);
+  }
+
+  return true;
 }
--- test/arithimetics.test.ts
+++ test/arithimetics.test.ts
@@ -14,19 +14,37 @@ testCalculate.title = (title: string = '', input: string, expected: number) => `
 parametrisedTest("parse sum operation", testCalculate, 
   { input: "(1+1)", expected: 2 },
   { input: "(2+2)", expected: 4 },
-  { input: "(4+3)", expected: 7 }
+  { input: "(4+3)", expected: 7 },
+  { input: "(10+3)", expected: 13 }
 );
 
 parametrisedTest("parse subtraction operation", testCalculate, 
-  { input: "(1-1)", expected: 0 },
+  { input: "(1-1)", expected: 0  },
+  { input: "(5-4)", expected: 1  },
+  { input: "(2-5)", expected: -3 },
+  { input: "(150-50)", expected: 100 }
 );
+
+parametrisedTest("parse multipication operation", testCalculate, 
+  { input: "(5*2)", expected: 10 },
+  { input: "(4*4)", expected: 16 },
+);
+
+parametrisedTest("parse division operation", testCalculate, 
+  { input: "(10/2)", expected: 5 },
+  { input: "(100/100)", expected: 1},
+);
+
+test("Division by zero should throw error", assert => {
+  assert.throws(() => calculate("(100/0)"));
+})
 
 interface TestData<InputType, ExpectedType> {
   input: InputType,
   expected: ExpectedType,
 }
 
-function parametrisedTest(title: string, macro: typeof testCalculate, ...data: TestData<string, number>[]) {
+function parametrisedTest(title: string, macro: typeof testCalculate, ...data: TestData<string, number>[]): void {
   data.forEach(testData => test(`${title}: ${testData}`, macro, testData.input, testData.expected));
 }
 
